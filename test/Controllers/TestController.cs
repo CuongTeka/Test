e13f182cbb377a7b9d70ca193faafc0ba40441d5
CuongTeka <cuongtk2711@gmail.com>
2024-06-16 21:26:18 +0700

    1

---
+++ test/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace test.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello from TestController!");
+        }
+
+        // GET api/test/{id}
+        //[HttpGet]
+        //[Route("api/test/{id:int}")]
+        //public IHttpActionResult Get(int id)
+        //{
+        //    return Ok($"You requested item {id}");
+        //}
+    }
+}
